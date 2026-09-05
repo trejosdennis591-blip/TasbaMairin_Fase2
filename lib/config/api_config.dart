e894d2403a289691196da4fc7a1632631e06a8a7
Dennis <trejosdennis591@gmail.com>
2026-09-04 20:40:48 -0600

    Actualiza panel administrativo

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static const String serverUrl =
-      'http://192.168.1.26:3000';
+      'http://192.168.1.32:3000';
 
   static const String baseUrl =
       '$serverUrl/api';
